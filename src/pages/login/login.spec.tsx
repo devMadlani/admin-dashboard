b000db5ab85f84df9e112af023cd26f348324025
Dev Madlani <madlanidev@gmail.com>
2026-01-26 08:14:55 +0530

    add test for login page

---
+++ src/pages/login/login.spec.tsx
@@ -0,0 +1,21 @@
+import { describe, expect, it } from "vitest";
+import { render, screen } from "@testing-library/react";
+import LoginPage from "./login";
+
+describe("Login Page", () => {
+  it("should render with required field", () => {
+    render(<LoginPage />);
+    // getBy   => throws an error
+    // findBy  =? async
+    // queryBy => return null
+
+    expect(screen.getByText("Sign in")).toBeInTheDocument();
+    expect(screen.getByPlaceholderText("Username")).toBeInTheDocument();
+    expect(screen.getByPlaceholderText("Password")).toBeInTheDocument();
+    expect(screen.getByRole("button", { name: "Log in" })).toBeInTheDocument();
+    expect(
+      screen.getByRole("checkbox", { name: "Remember me" }),
+    ).toBeInTheDocument();
+    expect(screen.getByText("Forgot password")).toBeInTheDocument();
+  });
+});
