e909f213b1b7b59906761027f1ab9744c499e17d
Dev Madlani <madlanidev@gmail.com>
2026-01-27 21:59:49 +0530

    add sidebar layout

--- src/layouts/Dashboard.tsx
+++ src/layouts/Dashboard.tsx
@@ -1,14 +1,86 @@
-import { Navigate, Outlet } from "react-router-dom";
+import { Navigate, NavLink, Outlet } from "react-router-dom";
 import { useAuthStore } from "../store";
+import { Layout, Menu, theme } from "antd";
+import Icon from "@ant-design/icons";
+import { useState } from "react";
+import Home from "../components/icons/Home";
+import UserIcon from "../components/icons/UserIcon";
+import { foodIcon } from "../components/icons/FoodIcon";
+import BasketIcon from "../components/icons/BasketIcon";
+import GiftIcon from "../components/icons/GiftIcon";
+
+const { Sider, Header, Content, Footer } = Layout;
+
+const items = [
+  {
+    key: "/",
+    icon: <Icon component={Home} />,
+    label: <NavLink to="/">Home</NavLink>,
+  },
+  {
+    key: "/users",
+    icon: <Icon component={UserIcon} />,
+    label: <NavLink to="/users">Users</NavLink>,
+  },
+  {
+    key: "restaurants",
+    icon: <Icon component={foodIcon} />,
+    label: <NavLink to="/restaurants">Restaurants</NavLink>,
+  },
+  {
+    key: "/products",
+    icon: <Icon component={BasketIcon} />,
+    label: <NavLink to="/products">Products</NavLink>,
+  },
+  {
+    key: "/promos",
+    icon: <Icon component={GiftIcon} />,
+    label: <NavLink to="/promos">Promos</NavLink>,
+  },
+];
 
 const Dashboard = () => {
   const { user } = useAuthStore();
+  const [collapsed, setCollapsed] = useState(false);
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
   if (!user) {
     return <Navigate to="/auth/login" replace={true} />;
   }
   return (
     <div>
-      <Outlet />
+      <Layout style={{ minHeight: "100vh" }}>
+        <Sider
+          collapsible
+          theme="light"
+          collapsed={collapsed}
+          onCollapse={(value) => setCollapsed(value)}
+        >
+          <div className="logo">
+            <img src="/icons/pizzaria-logo.svg" alt="" />
+          </div>
+          <Menu
+            theme="light"
+            defaultSelectedKeys={["/"]}
+            mode="inline"
+            items={items}
+          />
+        </Sider>
+        <Layout>
+          <Header style={{ padding: 0, background: colorBgContainer }} />
+          <Content style={{ margin: "0 16px" }}>
+            {/* <Breadcrumb
+              style={{ margin: "16px 0" }}
+              items={[{ title: "User" }, { title: "Bill" }]}
+            /> */}
+            <Outlet />
+          </Content>
+          <Footer style={{ textAlign: "center" }}>
+            Ant Design ©{new Date().getFullYear()} Created by Ant UED
+          </Footer>
+        </Layout>
+      </Layout>
     </div>
   );
 };
