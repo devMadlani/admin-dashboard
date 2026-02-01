cbc3408fd669aedc8a84efca828ca9e3286f1fff
Dev Madlani <madlanidev@gmail.com>
2026-02-01 12:25:26 +0530

    add table desing

--- src/pages/users/user.tsx
+++ src/pages/users/user.tsx
@@ -1,11 +1,41 @@
+import { Breadcrumb, Space, Table } from "antd";
 import { RightOutlined } from "@ant-design/icons";
-import { useQuery } from "@tanstack/react-query";
-import { Breadcrumb } from "antd";
 import { Link } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
 import { getUsers } from "../../http/api";
 import { User } from "../../../Types";
 
-const UserPage = () => {
+const columns = [
+  {
+    title: "ID",
+    dataIndex: "id",
+    key: "id",
+  },
+  {
+    title: "Name",
+    dataIndex: "firstName",
+    key: "firstName",
+    render: (_text: string, record: User) => {
+      return (
+        <div>
+          {record.firstName} {record.lastName}
+        </div>
+      );
+    },
+  },
+  {
+    title: "Email",
+    dataIndex: "email",
+    key: "email",
+  },
+  {
+    title: "Role",
+    dataIndex: "role",
+    key: "role",
+  },
+];
+
+const Users = () => {
   const {
     data: users,
     isLoading,
@@ -17,25 +47,21 @@ const UserPage = () => {
       return getUsers().then((res) => res.data);
     },
   });
+
   return (
     <>
-      <Breadcrumb
-        separator={<RightOutlined />}
-        items={[{ title: <Link to="/">Dashboard</Link> }, { title: "Users" }]}
-      />
-      {isLoading && <div>Loading...</div>}
-      {isError && <div>{error.message}</div>}
-      {users && (
-        <div>
-          <ul>
-            {users.map((user: User) => (
-              <li key={user.id}>{user.firstName}</li>
-            ))}
-          </ul>
-        </div>
-      )}
+      <Space direction="vertical" size="large" style={{ width: "100%" }}>
+        <Breadcrumb
+          separator={<RightOutlined />}
+          items={[{ title: <Link to="/">Dashboard</Link> }, { title: "Users" }]}
+        />
+        {isLoading && <div>Loading...</div>}
+        {isError && <div>{error.message}</div>}
+
+        <Table columns={columns} dataSource={users} />
+      </Space>
     </>
   );
 };
 
-export default UserPage;
+export default Users;
