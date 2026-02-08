4e6c672feb2c307a9e95186695e4d0a49763e52a
Dev Madlani <madlanidev@gmail.com>
2026-02-08 10:21:02 +0530

    add filters

---
+++ src/pages/products/Products.tsx
@@ -0,0 +1,32 @@
+import { Breadcrumb, Button, Flex, Form, Space } from "antd";
+import { PlusOutlined, RightOutlined } from "@ant-design/icons";
+import { Link } from "react-router-dom";
+import ProductsFilter from "./ProductsFilter";
+
+const Products = () => {
+  const [filterForm] = Form.useForm();
+  return (
+    <>
+      <Space direction="vertical" size="large" style={{ width: "100%" }}>
+        <Flex justify="space-between">
+          <Breadcrumb
+            separator={<RightOutlined />}
+            items={[
+              { title: <Link to="/">Dashboard</Link> },
+              { title: "Products" },
+            ]}
+          />
+        </Flex>
+        <Form form={filterForm} onFieldsChange={() => {}}>
+          <ProductsFilter>
+            <Button type="primary" icon={<PlusOutlined />} onClick={() => {}}>
+              Add Product
+            </Button>
+          </ProductsFilter>
+        </Form>
+      </Space>
+    </>
+  );
+};
+
+export default Products;
---
+++ src/pages/products/ProductsFilter.tsx
@@ -0,0 +1,75 @@
+import {
+  Card,
+  Col,
+  Form,
+  Input,
+  Row,
+  Select,
+  Space,
+  Switch,
+  Typography,
+} from "antd";
+
+type ProductsFilterProps = {
+  children?: React.ReactNode;
+};
+
+const ProductsFilter = ({ children }: ProductsFilterProps) => {
+  return (
+    <Card>
+      <Row justify="space-between">
+        <Col span={16}>
+          <Row gutter={20}>
+            <Col span={6}>
+              <Form.Item name="q" style={{ margin: 0 }}>
+                <Input.Search allowClear={true} placeholder="Search" />
+              </Form.Item>
+            </Col>
+
+            <Col span={6}>
+              <Form.Item name="category" style={{ margin: 0 }}>
+                <Select
+                  style={{ width: "100%" }}
+                  allowClear={true}
+                  placeholder="Select category"
+                >
+                  <Select.Option value="pizza">Pizza</Select.Option>
+                  <Select.Option value="beverages">Beverages</Select.Option>
+                </Select>
+              </Form.Item>
+            </Col>
+
+            <Col span={6}>
+              <Form.Item name="tenant" style={{ margin: 0 }}>
+                <Select
+                  style={{ width: "100%" }}
+                  allowClear={true}
+                  placeholder="Select restaurant"
+                >
+                  <Select.Option value="pizza">Pizza hub</Select.Option>
+                  <Select.Option value="beverages">Softy corner</Select.Option>
+                </Select>
+              </Form.Item>
+            </Col>
+
+            <Col span={6} style={{ alignSelf: "center" }}>
+              <Space>
+                <Switch defaultChecked onChange={() => {}} />
+                <Typography.Text
+                  style={{ width: "max-content", display: "block" }}
+                >
+                  Show only published
+                </Typography.Text>
+              </Space>
+            </Col>
+          </Row>
+        </Col>
+        <Col span={8} style={{ display: "flex", justifyContent: "end" }}>
+          {children}
+        </Col>
+      </Row>
+    </Card>
+  );
+};
+
+export default ProductsFilter;
