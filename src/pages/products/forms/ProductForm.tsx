d640381e39b0ef0e1a2a131a5ea2370149abb0bb
Dev Madlani <madlanidev@gmail.com>
2026-02-08 11:57:36 +0530

    refactor image component

--- src/pages/products/forms/ProductForm.tsx
+++ src/pages/products/forms/ProductForm.tsx
@@ -3,27 +3,21 @@ import {
   Col,
   Form,
   Input,
-  message,
   Row,
   Select,
   Space,
   Switch,
   Typography,
-  Upload,
-  UploadProps,
 } from "antd";
-import { PlusOutlined } from "@ant-design/icons";
+
 import { Category, Tenant } from "../../../types";
 import { useQuery } from "@tanstack/react-query";
 import { getCategories, getTenants } from "../../../http/api";
 import Pricing from "./Pricing";
 import Attributes from "./Attributes";
-import { useState } from "react";
+import ProductImage from "./ProductImage";
 
 const ProductForm = () => {
-  const [messageApi, contextHolder] = message.useMessage();
-  const [imageUrl, setImageUrl] = useState<string | null>(null);
-
   const selectedCategory = Form.useWatch("categoryId");
   console.log(selectedCategory);
   const { data: categories } = useQuery({
@@ -39,26 +33,6 @@ const ProductForm = () => {
       return getTenants(`perPage=100&currentPage=1`);
     },
   });
-
-  const uploaderConfig: UploadProps = {
-    name: "file",
-    multiple: false,
-    showUploadList: false,
-    beforeUpload: (file) => {
-      // Validation logic
-      const isJpgOrPng =
-        file.type === "image/jpeg" || file.type === "image/png";
-      if (!isJpgOrPng) {
-        console.error("You can only upload JPG/PNG file!");
-        messageApi.error("You can only upload JPG/PNG file!");
-      }
-
-      //todo:  size validation.
-      setImageUrl(URL.createObjectURL(file));
-
-      return false;
-    },
-  };
 
   return (
     <Row>
@@ -130,40 +104,15 @@ const ProductForm = () => {
               </Col>
             </Row>
           </Card>
-          <Card title="Product image" bordered={false}>
+          <Card title="Product image" variant="borderless">
             <Row gutter={20}>
               <Col span={12}>
-                <Form.Item
-                  label=""
-                  name="image"
-                  rules={[
-                    {
-                      required: true,
-                      message: "Please upload a product image",
-                    },
-                  ]}
-                >
-                  {contextHolder}
-                  <Upload listType="picture-card" {...uploaderConfig}>
-                    {imageUrl ? (
-                      <img
-                        src={imageUrl}
-                        alt="avatar"
-                        style={{ width: "100%" }}
-                      />
-                    ) : (
-                      <Space direction="vertical">
-                        <PlusOutlined />
-                        <Typography.Text>Upload</Typography.Text>
-                      </Space>
-                    )}
-                  </Upload>
-                </Form.Item>
+                <ProductImage />
               </Col>
             </Row>
           </Card>
 
-          <Card title="Tenant info" bordered={false}>
+          <Card title="Tenant info" variant="borderless">
             <Row gutter={24}>
               <Col span={24}>
                 <Form.Item
